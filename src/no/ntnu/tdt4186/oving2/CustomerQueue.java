6880e56411738b56321e667007cc4de386df3ce8
Emil Moltu Staurset <emil.staurset@gmail.cm>
2012-09-06 11:42:26 +0200

    endrer customerqueue, bruker lenket liste

--- src/no/ntnu/tdt4186/oving2/CustomerQueue.java
+++ src/no/ntnu/tdt4186/oving2/CustomerQueue.java
@@ -1,8 +1,14 @@
 package no.ntnu.tdt4186.oving2;
+
+import java.util.List;
+import java.util.LinkedList;
+
 /**
  * This class implements a queue of customers as a circular buffer.
  */
 public class CustomerQueue {
+	private LinkedList<Customer> list;
+	private int queueLength;
 	/**
 	 * Creates a new customer queue.
 	 * @param queueLength	The maximum length of the queue.
@@ -11,6 +17,26 @@ public class CustomerQueue {
     public CustomerQueue(int queueLength, Gui gui) {
 		// Incomplete
 	}
-
+    
+   
 	// Add more methods as needed
-}
+   
+   public synchronized void addCustomer(Customer c) throws InterruptedException {
+	   
+	   while (list.size() < queueLength){
+		    wait();
+		    list.add(c);
+		    notify();
+	   }
+   }
+   public synchronized Customer getFromQueueCustomer() throws InterruptedException {
+	   while(list.size() != 0) {
+		   wait();
+		  Customer c = list.removeFirst();
+		  notify();
+		  return c;
+	   }
+	   return null; 
+	   }
+   }
+
